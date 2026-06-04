af73a199ef0b6e753be49d29f91a4eebe3891a6a
Ashwath <ashwath0411@gmail.com>
2026-06-04 14:18:12 +0530

    Create mahasena odd or even problem.java

---
+++ mahasena odd or even problem.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int even = 0;
+        int odd = 0;
+
+        while(n-->0)
+        {
+            int x = sc.nextInt();
+
+            if(x % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        if(even > odd)
+            System.out.println("READY FOR BATTLE");
+        else
+            System.out.println("NOT READY");
+	}
+}
